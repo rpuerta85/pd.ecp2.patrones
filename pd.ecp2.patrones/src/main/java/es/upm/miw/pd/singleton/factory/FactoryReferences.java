76b9e387a14a55d97a7251bc6a8d274e9c585f65
raul <raul@raul-VAIO>
2014-10-13 01:05:46 +0200

    -Añadido singleton.factory

--- pd.ecp2.patrones/src/main/java/es/upm/miw/pd/singleton/factory/FactoryReferences.java
+++ pd.ecp2.patrones/src/main/java/es/upm/miw/pd/singleton/factory/FactoryReferences.java
@@ -1,16 +1,27 @@
 package es.upm.miw.pd.singleton.factory;
+
 import java.util.HashMap;
 import java.util.Map;
+
 public class FactoryReferences {
     private Map<String, Integer> references;
     private int reference;
+    private static FactoryReferences factory = null;
 
-    public FactoryReferences() {
+    private FactoryReferences() {
         this.references = new HashMap<>();
         this.reference = 0;
-}
+    }
 
-    public int getReference(String key) {
+    public static FactoryReferences getFactory(){
+        if (factory == null) {
+            factory = new FactoryReferences();
+        }
+        return factory;
+
+    }
+
+    public int getReference(final String key) {
         Integer result = this.references.get(key);
         if (result == null) {
             this.references.put(key, this.reference);
@@ -20,7 +31,8 @@ public class FactoryReferences {
         return result;
     }
 
-    public void removeReference(String key) {
+    public void removeReference(final String key) {
         this.references.remove(key);
     }
+
 }
