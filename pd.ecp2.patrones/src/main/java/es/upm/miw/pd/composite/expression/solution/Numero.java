4111c38e0a2b865144457fb9abe771bcca43ccc8
raul <raul@raul-VAIO>
2014-10-13 19:52:35 +0200

    Añadida interfaz publica de clase para la clase "Numero"

---
+++ pd.ecp2.patrones/src/main/java/es/upm/miw/pd/composite/expression/solution/Numero.java
@@ -0,0 +1,18 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public class Numero extends Expression{
+    private int numero;
+    
+    @Override
+    protected int operar() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    protected String toSring() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
