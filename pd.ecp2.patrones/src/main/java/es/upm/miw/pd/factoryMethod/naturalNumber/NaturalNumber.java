1987dae37a478afe90134fde65c891e79db9b069
raul <raul@raul-VAIO>
2014-10-16 21:18:36 +0200

    Añadida interfaz pública de clase, para la clase "NaturalNumber"

--- pd.ecp2.patrones/src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
+++ pd.ecp2.patrones/src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumber.java
@@ -2,4 +2,15 @@ package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public abstract class NaturalNumber {
 
+    protected int value;
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        assert value >= 0;
+        this.value = value;
+    }
 }
