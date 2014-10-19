dfa3cf70f1f1f988db8dfd370de3276e1cdd059e
raul <raul@raul-VAIO>
2014-10-19 18:11:23 +0200

    Añadida interfaz pública de clase para la clase "Texto"

---
+++ pd.ecp2.patrones/src/main/java/es/upm/miw/pd/text/Texto.java
@@ -0,0 +1,19 @@
+package es.upm.miw.pd.text;
+
+public class Texto extends Componente {
+
+    @Override
+    public String dibujar(boolean b) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(Componente h) {
+        // TODO Auto-generated method stub
+        
+    }
+
+   
+
+}
