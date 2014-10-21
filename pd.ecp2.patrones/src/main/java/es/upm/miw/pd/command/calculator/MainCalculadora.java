746895a6bdbd3bd095f12a86c6fdcc68bc7e3075
raul <raul@raul-VAIO>
2014-10-21 20:25:41 +0200

    Añadido clase "MainCalculadora"

---
+++ pd.ecp2.patrones/src/main/java/es/upm/miw/pd/command/calculator/MainCalculadora.java
@@ -0,0 +1,26 @@
+package es.upm.miw.pd.command.calculator;
+
+
+import upm.jbb.IO;
+
+public class MainCalculadora {
+    private GestorComandos gestor;
+
+    public MainCalculadora() {
+        Calculadora calc = new Calculadora();
+        this.gestor = new GestorComandos();
+        this.gestor.add(new ComandoSumar(calc));
+        this.gestor.add(new ComandoIniciar(calc));
+       
+        
+    }
+
+    public void ejecutar() {
+        String key = (String) IO.in.select(this.gestor.keys());
+        this.gestor.execute(key);
+    }
+
+    public static void main(String[] args) {
+        IO.in.addController(new MainCalculadora());
+    }
+}
